8b05676c5a7bed44f29ce31ebc34fbdb9f48f435
TideSofDarK <tidesmain@gmail.com>
2016-04-16 23:59:28 +0500

    Day 1

---
+++ Assets/Scripts/CloudSpawner.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CloudSpawner : MonoBehaviour {
+
+    public List<GameObject> cloudPrefabs;
+
+    public int density = 10;
+
+	void Start () {
+	    for (int i = 0; i < density; i++)
+        {
+            Vector3 newPos = Camera.main.ScreenToWorldPoint(new Vector3(
+                (Random.Range(-(float)Screen.width, (float)Screen.width*2)),
+                Random.Range((int)(Screen.height * 0.6), Screen.height*1.5f),
+                30));
+
+            newPos.z = -newPos.z;
+
+            GameObject go = (GameObject)Instantiate(cloudPrefabs[Random.Range(0, cloudPrefabs.Count-1)], newPos, 
+                Quaternion.identity);
+
+            go.GetComponent<SpriteRenderer>().flipX = Random.Range(0, 2) == 0;
+
+            var scale = Random.Range(3.0f, 4.5f);
+            go.transform.localScale = new Vector3(scale, scale, 0);
+
+            go.transform.SetParent(transform);
+
+            StartCoroutine("CloudMoving", go);
+        }
+
+        transform.Translate(new Vector3(0,0,33));
+	}
+
+    IEnumerator CloudMoving(GameObject go)
+    {
+        int direction = Random.Range(0, 2) == 0 ? 1 : -1;
+        float speed = Random.Range(0.005f, 0.2f);
+        while (true)
+        {
+            go.transform.Translate(new Vector3(direction,0,0) * Time.deltaTime * speed);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+	
+	void Update () {
+	    
+	}
+}
---
+++ Assets/Scripts/Fence.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fence : MonoBehaviour {
+
+    public GameObject fencePrefab;
+
+    public int count = 10;
+
+    private Sprite[] fences;
+
+	// Use this for initialization
+	void Start () {
+        fences = new Sprite[(count*2) + 1];
+        fences[0] = GetComponent<Sprite>();
+
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+
+        Vector2 spriteSize = new Vector2(sprite.bounds.size.x / transform.localScale.x, sprite.bounds.size.y / transform.localScale.y);
+
+        for (int i = 1; i < count * 2; i++)
+        {
+            int mod = i > count ? (i - count) * -1 : i;
+            GameObject go = (GameObject)Instantiate(fencePrefab, new Vector3(spriteSize.x * mod, 0,0) + transform.position, Quaternion.identity);
+            go.transform.SetParent(transform);
+            fences[i] = go.GetComponent<Sprite>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
